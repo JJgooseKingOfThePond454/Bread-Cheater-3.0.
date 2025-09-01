1b915fa81e47d91e0b56cb35c0b904bb2eda9600
JJgooseKingOfThePond454 <161132773+JJgooseKingOfThePond454@users.noreply.github.com>
2025-08-31 22:21:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -14,18 +14,4 @@ TbuttonElList.forEach(TbuttonEL => {
 //    var color = 'linear-gradient(90deg, rgb(147, 234, 240)' + x + '%,rgb(204, 233, 255)' + x +'%)';
 //    slider.style.background = color;
 //})
-const slider = document.querySelector('aggression-slider');
 
-    slider.addEventListener('input', function() {
-        const value = (this.value - this.min) / (this.max - this.min) * 100;
-        this.style.background = `linear-gradient(to right, #a7eaff 0%, #a7eaff ${value}%, #d3d3d3 ${value}%, #d3d3d3 100%)`;
-    });
-
-    const updateBackground = () => {
-        const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
-        slider.style.background = `linear-gradient(to right, #a7eaff 0%, #a7eaff ${value}%, #d3d3d3 ${value}%, #d3d3d3 100%)`;
-    };
-
-    updateBackground();
-
-    slider.addEventListener('input', updateBackground);
